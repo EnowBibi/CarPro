ac0d59ea4ebfa135167592f844f75a4528a24cc2
Enowtabi <enowbibi@gmail.com>
2024-12-07 04:00:34 +0100

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
-const  getStart= document.querySelector(".getStarted") ;
-getStart.addEventListener("click",()=>{
+const  getStarted= document.querySelector(".getStarted") ;
+const appointmentForm= document.querySelector(".appointment-form");
+getStarted.addEventListener("click",()=>{
     window.location.replace("signIn.html");
+})
+appointmentForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    window.alert("Congratulations!! Your appointment had been recorder successfully!!!");
 })
